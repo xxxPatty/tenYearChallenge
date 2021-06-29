68cd87da4eab8d94b554bfe3def3e90fa109dc71
Patty <L224749391@gmail.com>
2021-06-29 18:00:04 +0800

    Initial Commit

---
+++ tenYearChallenge/tenYearChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tenYearChallengeApp.swift
+//  tenYearChallenge
+//
+//  Created by 林湘羚 on 2021/6/29.
+//
+
+import SwiftUI
+
+@main
+struct tenYearChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
